f6fdae74774bbdf3f74a11efb3966210976c0001
mikemskim <mskim@steconsulting.co.kr>
2019-11-12 21:01:06 +0900

    getItem 메소드를 코틀린 문법으로 변경 When을 사용해서 상황에 따라 다른 리턴값 처리되게 작성

--- app/src/main/java/com/tj/a20191112_01_fragment/adapters/TestViewPagerAdapter.kt
+++ app/src/main/java/com/tj/a20191112_01_fragment/adapters/TestViewPagerAdapter.kt
@@ -10,10 +10,18 @@ class TestViewPagerAdapter(fm:FragmentManager) : FragmentPagerAdapter(fm) {
 
 //    각 위치에 어떤 fragment를 보여줄 건지
     override fun getItem(position: Int): Fragment {
-        if (position == 0) {
-            return FirstFragment()
-        } else {
-            return SecondFragment()
+//        if (position == 0) {
+//            return FirstFragment()
+//        } else {
+//            return SecondFragment()
+//        }
+        return when(position) {
+            0 -> {
+                FirstFragment()
+            }
+            else -> {
+                SecondFragment()
+            }
         }
 
     }
