13f5182840374aaa112abbf71271815447bf53aa
mikemskim <mskim@steconsulting.co.kr>
2019-11-12 19:35:38 +0900

    Initial commit

---
+++ app/src/main/java/com/tj/a20191112_01_fragment/BaseActivity.kt
@@ -0,0 +1,8 @@
+package com.tj.a20191112_01_fragment
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
---
+++ app/src/main/java/com/tj/a20191112_01_fragment/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.tj.a20191112_01_fragment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+    }
+
+    override fun setValues() {
+    }
+
+
+}
