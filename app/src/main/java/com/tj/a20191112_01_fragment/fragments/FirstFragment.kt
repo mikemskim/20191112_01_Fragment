270252d2b3cd4a5287022eb42a1a077a63a827b8
mikemskim <mskim@steconsulting.co.kr>
2019-11-12 19:57:12 +0900

    첫 프레그먼트 코딩  - onActivityCreated에서 작성

--- app/src/main/java/com/tj/a20191112_01_fragment/fragments/FirstFragment.kt
+++ app/src/main/java/com/tj/a20191112_01_fragment/fragments/FirstFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.tj.a20191112_01_fragment.R
+import kotlinx.android.synthetic.main.fragment_first.*
 
 class FirstFragment : Fragment() {
 
@@ -15,5 +16,13 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+//    액티비티 위에 얹어졌을 때 실행되는 fun
+//    실질적으로 fragment 입장에서는 onCreate와 마찬가지.
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        firstFragTxt.text = "프레그먼트에서 글자를 바꿈"
     }
 }
